7cc53b2a01b8b0bd675fa32ca8fe5ae9a9acf0e6
Slava Barinov <rayslava@rayslava.com>
2025-09-16 14:21:13 +0900

    build: Add PATH setup in dir-locals to fix rust-analyzer
    The rust-analyzer itself can be created with
    
    ln -sf \
     ~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin/rust-analyzer \
     ~/.rustup/toolchains/esp/bin/rust-analyzer

---
+++ .dir-locals.el
@@ -0,0 +1,15 @@
+;;; Update PATH from esp exports into local process environment
+((nil .
+      ;;; ((lsp-rust-analyzer-proc-macro-enable . nil))
+      ((eval .
+	     (progn
+	       (make-local-variable 'process-environment)
+	       (with-temp-buffer
+		 (call-process "bash" nil t nil "-c"
+			       "source ~/export-esp.sh; env | egrep '^PATH='")
+		 (goto-char (point-min))
+		 (while (not (eobp))
+		   (setq process-environment
+			 (cons (buffer-substring (point) (line-end-position))
+			       process-environment))
+		   (forward-line 1))))))))
